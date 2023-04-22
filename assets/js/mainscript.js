c1207ee4b981857725f7fb8d72480a1eed8ce1a5
AipuAmeh <aipuameh.16@gmail.com>
2023-04-21 20:11:56 -0400

    added beginning html, obtained api key

---
+++ assets/js/mainscript.js
@@ -0,0 +1,4 @@
+var apiKey = "485d8029f9463fb9427a7793559846d9";
+//user input concatenated into a string
+var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
