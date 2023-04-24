90bfb2b07799a3f460dce50e9d88a21946ff9af8
AipuAmeh <aipuameh.16@gmail.com>
2023-04-23 20:04:06 -0400

    added current weather conditions when typing in city

--- assets/js/mainscript.js
+++ assets/js/mainscript.js
@@ -3,6 +3,8 @@ var apiKey = "485d8029f9463fb9427a7793559846d9";
 var city = document.getElementById("user-input");
 var search = document.getElementById("search");
 
+//render weather data at top of page
+
 
 //when you click search the city is stored
 //displaying city info based off user input
@@ -21,14 +23,50 @@ search.addEventListener("click", function(event) {
         })
         .then(function(data) {
             console.log(data);
-                  
+            var featuredWeather = document.querySelector(".weather-features");
+            //city name and date in MM/DD/YYYY format
+          //temp, wind, humidity
+            // var date = dayjs().format("M/DD/YYYY");
+           var cityName = document.createElement("h2");
+            cityName.textContent = data.name;
+            // ("#current-time").text(date);
+            // console.log(date);
+            console.log(data.name);
+            featuredWeather.append(cityName);
+            
+            var weatherDetails = document.querySelector("#weather-details");
+
+            var temp = document.createElement("li");
+            temp.textContent = "Temp: " + data.main.temp + " \xB0" + "F";
+            weatherDetails.append(temp);
+            featuredWeather.append(weatherDetails);
+
+            var humidity = document.createElement("li");
+            humidity.textContent = "Humidity: " + data.main.humidity + " %";
+            weatherDetails.append(humidity)
+            featuredWeather.append(weatherDetails);
+
+            var wind = document.createElement("li");
+            wind.textContent = "Wind: " + data.wind.speed + " MPH";
+            weatherDetails.append(wind);
+            featuredWeather.append(weatherDetails);
+            
         })
         localStorage.setItem("cityInput", cityInput);
+       
+
+// function renderWeather() {
+//     // console.log(weather);
+   
+  
+    
+// }
+// renderWeather();
 }) 
 
-//render weather data
-function renderWeather() {
-    document.querySelector("#weather-forecast")
-}
+//function for 5 day forecast
+//new api url to be called 
+
+
 
 
